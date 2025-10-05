92b67106805dc8159c87f495a8208641453641f8
evgTSV <freefiery@gmail.com>
2025-10-05 22:12:50 +0500

    {fix video}

---
+++ ci-github-pages.ps1
@@ -0,0 +1,54 @@
+﻿$msg = Read-Host -Prompt "Введите название изменения"
+
+git fetch
+
+Write-Host "Обновление ветки main" -ForegroundColor Blue
+
+git checkout main
+
+git add .
+git commit -m "{$msg}"
+
+Write-Host "Обновление ветки main: Успех" -ForegroundColor Green
+
+Write-Host "Сборка и восстановление зависимостей" -ForegroundColor Blue
+
+npm i
+nmp exec yarn
+nmp exec yarn build
+
+Write-Host "Сборка и восстановление зависимостей: Успех" -ForegroundColor Green
+
+Write-Host "Обновление ветки deploy" -ForegroundColor Blue
+
+git checkout deploy
+
+$index = "index.html"
+$assets = "assets"
+
+Write-Host "Удаление старых файлов" -ForegroundColor Blue
+
+if (Test-Path -Path $index) {
+    Remove-Item -LiteralPath $index -Force -Confirm:$false
+}
+
+if (test-Path -Path $assets) {
+    Remove-Item -LiteralPath $assets -Force -Recurse -Confirm:$false 
+}
+
+Write-Host "Копирование новых файлов сборки" -ForegroundColor Blue
+
+Copy-Item -Path ".\build\*" -Destination ".\"
+Remove-Item -Path ".\build" -Force -Recurse -Confirm:$false
+
+$commitId = New-Guid
+git add .
+git commit -m "build-$commitId"
+
+Write-Host "Обновление ветки deploy: Успех" -ForegroundColor Green
+
+Write-Host "Публикация..." -ForegroundColor Blue
+
+git push
+
+git checkout main
